ea3d76ec1b14e67ffeac361dc87dae8dc3e175ed
Andrew <andrewa66@icloud.com>
2024-10-04 16:52:34 -0400

    Created function to order the standings

--- script.js
+++ script.js
@@ -5,6 +5,26 @@ const conferenceTeams = {
     west: ["Ballers", "Crusaders", "Dinos", "Dogs", "Explorers", "Frogs", "Jungle", "Knights", "Magicians", "Missiles", "Phantoms", "Pirates", "Scorpions", "Turtles", "Vipers", "Wave"]
 };
 
+const schedule = {
+    day: 1,
+    1: [[1, 2], [8, 10], [9, 16], [7, 11], [3, 15], [6, 12], [4, 14], [5, 13]],
+    2: [[9, 10], [1, 3], [8, 11], [2, 16], [7, 12], [4, 15], [6, 13], [5, 14]],
+    3: [[2, 3], [9, 11], [1, 4], [8, 12], [10, 16], [7, 13], [5, 15], [6, 14]],
+    4: [[10, 11], [2, 4], [9, 12], [1, 5], [8, 13], [3, 16], [7, 14], [6, 15]],
+    5: [[3, 4], [10, 12], [2, 5], [9, 13], [1, 6], [8, 14], [11, 16], [7, 15]],
+    6: [[11, 12], [3, 5], [10, 13], [2, 6], [9, 14], [1, 7], [8, 15], [4, 16]],
+    7: [[4, 5], [11, 13], [3, 6], [10, 14], [2, 7], [9, 15], [1, 8], [12, 16]],
+    8: [[13, 12], [4, 6], [11, 14], [3, 7], [10, 15], [2, 8], [5, 16], [1, 9]],
+    9: [[5, 6], [12, 14], [4, 7], [11, 15], [3, 8], [13, 16], [2, 9], [1, 10]],
+    10: [[13, 14], [5, 7], [12, 15], [4, 8], [6, 16], [3, 9], [1, 11], [2, 10]],
+    11: [[6, 7], [13, 15], [5, 8], [14, 16], [4, 9], [1, 12], [3, 10]],
+    12: [[2, 11], [14, 15], [6, 8], [7, 16], [5, 9], [1, 13], [4, 10]],
+    13: [[2, 12], [3, 11], [7, 8], [15, 16], [6, 9], [1, 14], [5, 10]],
+    14: [[2, 13], [4, 11], [3, 12], [8, 16], [7, 9], [1, 15], [6, 10]],
+    15: [[2, 14], [5, 11], [3, 13], [4, 12], [8, 9], [1, 16], [7, 10]],
+    16: [[2, 15], [6, 11], [3, 14], [5, 12], [4, 13]] 
+};
+
 const teams = {};
 
 class Team {
@@ -86,6 +106,73 @@ function displayStandings() {
     }
 }
 
+function displaySchedule() {
+    if (document.getElementById("schedule-div")) {
+        const scheduleDiv = document.getElementById("schedule-div");
+        scheduleDiv.remove();
+    }
+    const scheduleDiv = document.createElement("div");
+    const scheduleTitle = document.createElement("div");
+    const scheduleSection = document.createElement("div");
+    scheduleDiv.id = "schedule-div";
+    scheduleTitle.id = "schedule-title";
+    scheduleSection.id = "schedule-section";
+    scheduleTitle.innerHTML = "Schedule";
+    document.getElementById("container").append(scheduleDiv);
+    document.getElementById("schedule-div").append(scheduleTitle);
+    document.getElementById("schedule-div").append(scheduleSection);
+    if (schedule.day <= 16) {
+        const day = schedule.day.toString();
+        for (const conference in teams) {
+            const conferenceDiv = document.createElement("div");
+            const conferenceTitle = document.createElement("div");
+            conferenceDiv.id = conference + "-schedule";
+            conferenceTitle.id = "conference-title";
+            document.getElementById("schedule-div").append(conferenceDiv);
+            document.getElementById(conference + "-title").append(conferenceTitle);
+            for (let i = 0; i < schedule[day].length; i++) {
+                let team1;
+                let team2;
+                let record1;
+                let record2;
+                for (const team in teams[conference]) {
+                    if (teams[conference][team].teamNum === schedule[day][i][0]) {
+                        team1 = team;
+                        record1 = "(" + teams[conference][team].wins + "-" + teams[conference][team].losses + ")";
+                    } else if (teams[conference][team].teamNum === schedule[day][i][1]) {
+                        team2 = team;
+                        record2 = "(" + teams[conference][team].wins + "-" + teams[conference][team].losses + ")";
+                    }
+                }
+                let matchup = document.createElement("div");
+                matchup.innerHTML = team1 + " " + record1 + " vs. " + team2 + " " + record2;
+                document.getElementById(conference + "-schedule").append(matchup);
+            }
+        } 
+    } else {
+        document.getElementById("schedule-div").innerHTML = "No more regular season games";
+    }
+}
+
+function orderStandings() {
+    for (const conference in teams) {
+        let teamsArr = [];
+        for (const team in teams[conference]) {
+            teamsArr.push({name: teams[conference][team].name, wins: teams[conference][team].wins, standing: teams[conference][team].standing});
+        }
+        teamsArr.sort(function(a, b) {
+            return b.wins - a.wins;
+        });
+        for (let i = 1; i < teamsArr.length + 1; i++) {
+            for (const team in teams[conference]) {
+                if (teams[conference][team].name === teamsArr[i - 1].name) {
+                    teams[conference][team].standing = i;
+                }
+            }
+        }
+    }
+}
+
 /*
 function simulatePlayerScore(player) {
     const randomNum = Math.random() * 100;
@@ -143,3 +230,4 @@ function simulateGame() {
 */
 createLeague();
 displayStandings();
+displaySchedule();
